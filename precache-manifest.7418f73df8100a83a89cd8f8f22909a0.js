214c38c21ff6a7e9ddf65c9ae3e476b4140959f2
Victor Chung <victochung998@yahoo.com>
2019-11-09 16:01:24 -0800

    Updates

---
+++ precache-manifest.7418f73df8100a83a89cd8f8f22909a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277669b52e76490985a5e95f05efa08a",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "4729b645405592226e78",
+    "url": "/clicky-game/static/css/main.0565df6a.chunk.css"
+  },
+  {
+    "revision": "49ef6d7d0827c0599ecb",
+    "url": "/clicky-game/static/js/2.98690820.chunk.js"
+  },
+  {
+    "revision": "4729b645405592226e78",
+    "url": "/clicky-game/static/js/main.791d6f4a.chunk.js"
+  },
+  {
+    "revision": "6e3658582142cd6ca0be",
+    "url": "/clicky-game/static/js/runtime-main.5997d276.js"
+  }
+]);
